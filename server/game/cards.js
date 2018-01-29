6f6216b8b3a29bd86856ca09354ea3e2bd39230d
alewando <levandosky@gmail.com>
2018-01-29 19:00:26 +0100

    Have to set card type enum

--- server/game/cards.js
+++ server/game/cards.js
@@ -1,266 +1,266 @@
 export const cards = [
 	{
-		"Rodzaj": "NieNieNie",
-		"Opis": "Nakarm swojego przeciwnika kanapką z rezygnacją i polej ją specjlanym sosem o smaku ",
+		"type": "NieNieNie",
+		"description": "Nakarm swojego przeciwnika kanapką z rezygnacją i polej ją specjlanym sosem o smaku ",
 		"Akcja": "Przerwij akcję innego gracza. Możesz użyć tej karty w dowolnym momencie",
 		"AlternatywnyOpis": "Wylej kawę na komputer gracza",
 		"id": "4"
 	},
 	{
-		"Rodzaj": "NieNieNie",
-		"Opis": "Wygrywasz pokojową nagrodę NIEBLA",
+		"type": "NieNieNie",
+		"description": "Wygrywasz pokojową nagrodę NIEBLA",
 		"Akcja": "Przerwij akcję innego gracza. Możesz użyć tej karty w dowolnym momencie",
 		"AlternatywnyOpis": "A zajebać Ci'",
 		"id": "5"
 	},
 	{
-		"Rodzaj": "NieNieNie",
-		"Opis": "NIENJA sprzedał Ci smoczego kopniaka",
+		"type": "NieNieNie",
+		"description": "NIENJA sprzedał Ci smoczego kopniaka",
 		"Akcja": "Przerwij akcję innego gracza. Możesz użyć tej karty w dowolnym momencie",
 		"AlternatywnyOpis": "Może zrobiłbyś rekrutacje za 10 minut",
 		"id": "6"
 	},
 	{
-		"Rodzaj": "NieNieNie",
-		"Opis": "NIESTRADAMUS mówi jak jest",
+		"type": "NieNieNie",
+		"description": "NIESTRADAMUS mówi jak jest",
 		"Akcja": "Przerwij akcję innego gracza. Możesz użyć tej karty w dowolnym momencie",
 		"AlternatywnyOpis": "",
 		"id": "7"
 	},
 	{
-		"Rodzaj": "NieNieNie",
-		"Opis": "Do pokoju wbiega Daniel",
+		"type": "NieNieNie",
+		"description": "Do pokoju wbiega Daniel",
 		"Akcja": "Przerwij akcję innego gracza. Możesz użyć tej karty w dowolnym momencie",
 		"AlternatywnyOpis": " ",
 		"id": "8"
 	},
 	{
-		"Rodzaj": "Potasuj",
-		"Opis": "Znikąd pojawia się międzynardodowa kuweta",
+		"type": "Potasuj",
+		"description": "Znikąd pojawia się międzynardodowa kuweta",
 		"Akcja": "Potasuj talię",
 		"AlternatywnyOpis": "Puszczasz builda",
 		"id": "10"
 	},
 	{
-		"Rodzaj": "Potasuj",
-		"Opis": "Abrakrab Lincoln zostaje prezydentem",
+		"type": "Potasuj",
+		"description": "Abrakrab Lincoln zostaje prezydentem",
 		"Akcja": "Potasuj talię",
 		"AlternatywnyOpis": " ",
 		"id": "11"
 	},
 	{
-		"Rodzaj": "Potasuj",
-		"Opis": "Zza chmur wylatuje desant pierdopierzy",
+		"type": "Potasuj",
+		"description": "Zza chmur wylatuje desant pierdopierzy",
 		"Akcja": "Potasuj talię",
 		"AlternatywnyOpis": " ",
 		"id": "12"
 	},
 	{
-		"Rodzaj": "Potasuj",
-		"Opis": "Znad wchodniego morza nadciągnęła burza elektromagnetyczna",
+		"type": "Potasuj",
+		"description": "Znad wchodniego morza nadciągnęła burza elektromagnetyczna",
 		"Akcja": "Potasuj talię",
 		"AlternatywnyOpis": " ",
 		"id": "13"
 	},
 	{
-		"Rodzaj": "Przysługa",
-		"Opis": "Zostań sługusem imprezujących wiewiórek",
+		"type": "Przysługa",
+		"description": "Zostań sługusem imprezujących wiewiórek",
 		"Akcja": "Jeden z graczy musi Ci oddać wybraną przez siebie kartę",
 		"AlternatywnyOpis": " ",
 		"id": "15"
 	},
 	{
-		"Rodzaj": "Przysługa",
-		"Opis": "Zabierz swoich przyjaciół na brodowyprawę swoją brodołódką ",
+		"type": "Przysługa",
+		"description": "Zabierz swoich przyjaciół na brodowyprawę swoją brodołódką ",
 		"Akcja": "Jeden z graczy musi Ci oddać wybraną przez siebie kartę",
 		"AlternatywnyOpis": " ",
 		"id": "16"
 	},
 	{
-		"Rodzaj": "Przysługa",
-		"Opis": "Poproś kogoś",
+		"type": "Przysługa",
+		"description": "Poproś kogoś",
 		"Akcja": " żeby umył włosy",
 		"AlternatywnyOpis": " które rosną Ci na plecach",
 		"id": "Jeden z graczy musi Ci oddać wybraną przez siebie kartę"
 	},
 	{
-		"Rodzaj": "Przysługa",
-		"Opis": "Wysmaruj swój brzuch masłem orzechowym i zdobądź nowych przyjaciół ",
+		"type": "Przysługa",
+		"description": "Wysmaruj swój brzuch masłem orzechowym i zdobądź nowych przyjaciół ",
 		"Akcja": "Jeden z graczy musi Ci oddać wybraną przez siebie kartę",
 		"AlternatywnyOpis": " ",
 		"id": "18"
 	},
 	{
-		"Rodzaj": "Pomiń",
-		"Opis": "Uruchom napęd hiperkozi",
+		"type": "Pomiń",
+		"description": "Uruchom napęd hiperkozi",
 		"Akcja": "Zakończ turę bez dobierania karty",
 		"AlternatywnyOpis": "To nie ma nic wspólnego z moją funkcjonalnością",
 		"id": "20"
 	},
 	{
-		"Rodzaj": "Pomiń",
-		"Opis": "Ujeżdżaj królikoraptora",
+		"type": "Pomiń",
+		"description": "Ujeżdżaj królikoraptora",
 		"Akcja": "Zakończ turę bez dobierania karty",
 		"AlternatywnyOpis": "Cannot reproduce",
 		"id": "21"
 	},
 	{
-		"Rodzaj": "Pomiń",
-		"Opis": "Wmów krabom",
+		"type": "Pomiń",
+		"description": "Wmów krabom",
 		"Akcja": " że jesteś jednym z nich",
 		"AlternatywnyOpis": "Zakończ turę bez dobierania karty",
 		"id": ""
 	},
 	{
-		"Rodzaj": "Pomiń",
-		"Opis": "Przywdziej gepardzie gatki",
+		"type": "Pomiń",
+		"description": "Przywdziej gepardzie gatki",
 		"Akcja": "Zakończ turę bez dobierania karty",
 		"AlternatywnyOpis": "",
 		"id": "23"
 	},
 	{
-		"Rodzaj": "Co kryje przyszlość",
-		"Opis": "Dowódź jednostą specjlaną krolików",
+		"type": "Co kryje przyszlość",
+		"description": "Dowódź jednostą specjlaną krolików",
 		"Akcja": "Dyskretnie obejrzyj 3 wierzchnie karty z talii",
 		"AlternatywnyOpis": "Dzwonisz do supportu",
 		"id": "25"
 	},
 	{
-		"Rodzaj": "Co kryje przyszlość",
-		"Opis": "Pomiziaj po brzuchu PROSIOROŻCA",
+		"type": "Co kryje przyszlość",
+		"description": "Pomiziaj po brzuchu PROSIOROŻCA",
 		"Akcja": "Dyskretnie obejrzyj 3 wierzchnie karty z talii",
 		"AlternatywnyOpis": " ",
 		"id": "26"
 	},
 	{
-		"Rodzaj": "Co kryje przyszlość",
-		"Opis": "Wezwij rawkę błazna",
+		"type": "Co kryje przyszlość",
+		"description": "Wezwij rawkę błazna",
 		"Akcja": "Dyskretnie obejrzyj 3 wierzchnie karty z talii",
 		"AlternatywnyOpis": " ",
 		"id": "27"
 	},
 	{
-		"Rodzaj": "Co kryje przyszlość",
-		"Opis": "Spałąszuj enchiladę z mięsem jednorożca i pozyskaj jej węglowodanowe moce",
+		"type": "Co kryje przyszlość",
+		"description": "Spałąszuj enchiladę z mięsem jednorożca i pozyskaj jej węglowodanowe moce",
 		"Akcja": "Dyskretnie obejrzyj 3 wierzchnie karty z talii",
 		"AlternatywnyOpis": " ",
 		"id": "28"
 	},
 	{
-		"Rodzaj": "Co kryje przyszlość",
-		"Opis": "Zadaj pytanie wszechwidzącemu koziemu magowi",
+		"type": "Co kryje przyszlość",
+		"description": "Zadaj pytanie wszechwidzącemu koziemu magowi",
 		"Akcja": "Dyskretnie obejrzyj 3 wierzchnie karty z talii",
 		"AlternatywnyOpis": " ",
 		"id": "29"
 	},
 	{
-		"Rodzaj": "Atakuj",
-		"Opis": "Uwolnij kotołaka",
+		"type": "Atakuj",
+		"description": "Uwolnij kotołaka",
 		"Akcja": "Zakończ turę bez dobierania karty następnie gracz musi zagrać 2 razy",
 		"AlternatywnyOpis": "Babrzesz się w gównie",
 		"id": "31"
 	},
 	{
-		"Rodzaj": "Atakuj",
-		"Opis": "Obudź misiozaura",
+		"type": "Atakuj",
+		"description": "Obudź misiozaura",
 		"Akcja": "Zakończ turę bez dobierania karty następnie gracz musi zagrać 2 razy",
 		"AlternatywnyOpis": "Atak QuAka",
 		"id": "32"
 	},
 	{
-		"Rodzaj": "Atakuj",
-		"Opis": "Odpal krabapultę",
+		"type": "Atakuj",
+		"description": "Odpal krabapultę",
 		"Akcja": "Zakończ turę bez dobierania karty następnie gracz musi zagrać 2 razy",
 		"AlternatywnyOpis": " ",
 		"id": "33"
 	},
 	{
-		"Rodzaj": "Atakuj",
-		"Opis": "Odpal tysiącletnie włosy na plecach",
+		"type": "Atakuj",
+		"description": "Odpal tysiącletnie włosy na plecach",
 		"Akcja": "Zakończ turę bez dobierania karty następnie gracz musi zagrać 2 razy",
 		"AlternatywnyOpis": " ",
 		"id": "34"
 	},
 	{
-		"Rodzaj": "Kot TĘCZORZYGOWY",
-		"Opis": "",
+		"type": "Kot TĘCZORZYGOWY",
+		"description": "",
 		"Akcja": "Ta kocia karta nie działą w pojedynkę. Zagraj 2 koty tęczorzygowe jako parę aby ukraść losową kartę od innego gracza",
 		"AlternatywnyOpis": " ",
 		"id": "36"
 	},
 	{
-		"Rodzaj": "Kotowoc arbuzowaty",
-		"Opis": "",
+		"type": "Kotowoc arbuzowaty",
+		"description": "",
 		"Akcja": "Ta kocia karta nie działą w pojedynkę. Zagraj 2 kotowoce jako parę aby ukraść losową kartę od innego gracza",
 		"AlternatywnyOpis": "It's okej to be QAEJ",
 		"id": "38"
 	},
 	{
-		"Rodzaj": "Tacokot",
-		"Opis": "",
+		"type": "Tacokot",
+		"description": "",
 		"Akcja": "Ta kocia karta nie działą w pojedynkę. Zagraj 2 tacokoty jako parę aby ukraść losową kartę od innego gracza",
 		"AlternatywnyOpis": "Pan kanapka",
 		"id": "40"
 	},
 	{
-		"Rodzaj": "Kartoflokot włochaty",
-		"Opis": "",
+		"type": "Kartoflokot włochaty",
+		"description": "",
 		"Akcja": "Ta kocia karta nie działą w pojedynkę. Zagraj 2 kartoflokoty jako parę aby ukraść losową kartę od innego gracza",
 		"AlternatywnyOpis": " ",
 		"id": "42"
 	},
 	{
-		"Rodzaj": "Brodokot",
-		"Opis": "",
+		"type": "Brodokot",
+		"description": "",
 		"Akcja": "Ta kocia karta nie działą w pojedynkę. Zagraj 2 brodokoty jako parę aby ukraść losową kartę od innego gracza",
 		"AlternatywnyOpis": " ",
 		"id": "44"
 	},
 	{
-		"Rodzaj": "Rozbrój",
-		"Opis": "Kototerapia",
+		"type": "Rozbrój",
+		"description": "Kototerapia",
 		"Akcja": "Umieść ostatnio wyciągniętą kartę z powrotem w talii",
 		"AlternatywnyOpis": "Świć pomaga Ci w zadaniu",
 		"id": "46"
 	},
 	{
-		"Rodzaj": "Rozbrój",
-		"Opis": "Kanapkami z kocimiętką",
+		"type": "Rozbrój",
+		"description": "Kanapkami z kocimiętką",
 		"Akcja": "Umieść ostatnio wyciągniętą kartę z powrotem w talii",
 		"AlternatywnyOpis": " ",
 		"id": "47"
 	},
 	{
-		"Rodzaj": "Rozbrój",
-		"Opis": "Gazami o 3 w nocy",
+		"type": "Rozbrój",
+		"description": "Gazami o 3 w nocy",
 		"Akcja": "Umieść ostatnio wyciągniętą kartę z powrotem w talii",
 		"AlternatywnyOpis": " ",
 		"id": "48"
 	},
 	{
-		"Rodzaj": "Rozbrój",
-		"Opis": "Mizianiem kota po brzuchu",
+		"type": "Rozbrój",
+		"description": "Mizianiem kota po brzuchu",
 		"Akcja": "Umieść ostatnio wyciągniętą kartę z powrotem w talii",
 		"AlternatywnyOpis": " ",
 		"id": "49"
 	},
 	{
-		"Rodzaj": "Rozbrój",
-		"Opis": "Laserowym wskaźnikiem",
+		"type": "Rozbrój",
+		"description": "Laserowym wskaźnikiem",
 		"Akcja": "Umieść ostatnio wyciągniętą kartę z powrotem w talii",
 		"AlternatywnyOpis": " ",
 		"id": "50"
 	},
 	{
-		"Rodzaj": "Rozbrój",
-		"Opis": "Kocią jogą ",
+		"type": "Rozbrój",
+		"description": "Kocią jogą ",
 		"Akcja": "Umieść ostatnio wyciągniętą kartę z powrotem w talii",
 		"AlternatywnyOpis": " ",
 		"id": "51"
 	},
 	{
-		"Rodzaj": "Eksplodujący kotek",
-		"Opis": "",
+		"type": "Eksplodujący kotek",
+		"description": "",
 		"Akcja": "Natychmiastowo ujawnij tę kartę",
 		"AlternatywnyOpis": " ",
 		"id": "53"
