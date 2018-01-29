63b9daaf356cfce48d5e3f3a042b34e9efc80b7d
Christian <christiangrobelski@gmail.com>
2018-01-29 10:48:46 +0100

    Added redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux'
+import RootReducer from './reducers/RootReducer'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(RootReducer);
+
+ReactDOM.render(<App store={store}/>, document.getElementById('root'));
 registerServiceWorker();
