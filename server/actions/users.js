6ee0d3da9de536689e1670a88ada88658bce321d
Christian <christiangrobelski@gmail.com>
2018-01-30 10:24:17 +0100

    added getting cards

--- server/actions/users.js
+++ server/actions/users.js
@@ -1,31 +1,52 @@
 import Game from '../game';
 
 const SERVER_LOGIN = 'SERVER/LOGIN';
+const SERVER_GET_CARD = 'SERVER/GET_CARD';
 
 
 const usersActions = {
-  [SERVER_LOGIN] : (all, user, action) => {
-    const newUser = Game.addPlayer({sessionId: user.id, name: action.payload});
+    [SERVER_LOGIN]: (all, user, action) => {
+        const newUser = Game.addPlayer({sessionId: user.id, name: action.payload});
 
-    user.emit('action', {
-      type: 'SET_USER',
-      payload: Object.assign({
-        logged: true
-      }, newUser)
-    });
+        user.emit('action', {
+            type: 'SET_USER',
+            payload: Object.assign({
+                logged: true
+            }, newUser)
+        });
 
-    all.emit('action', {
-      type: 'SET_PLAYERS_LIST',
-      payload: {
-        list: Game.getPlayers()
-      }
-    });
+        all.emit('action', {
+            type: 'SET_PLAYERS_LIST',
+            payload: {
+                list: Game.getPlayers()
+            }
+        });
 
-    all.emit('action', {
-      type: 'SET_DECK_SIZE',
-      payload: Game.getDeckSize()
-    })
-  }
+        all.emit('action', {
+            type: 'SET_DECK_SIZE',
+            payload: Game.getDeckSize()
+        })
+    },
+    [SERVER_GET_CARD]: (all, user, action) => {
+        Game.getCard(user);
+        const player = Game.getPlayer(user);
+
+        user.emit('action', {
+            type: 'SET_USER',
+            payload: Object.assign({
+                logged: true
+            }, player)
+        });
+
+        all.emit('action', {
+            type: 'SET_PLAYERS_LIST',
+            payload: {
+                list: Game.getPlayers()
+            }
+        });
+
+    }
+
 }
 
 
--- server/game/index.js
+++ server/game/index.js
@@ -4,77 +4,95 @@ import {findKey, mapValues, values} from 'lodash';
 import {cards} from './cards';
 
 const gameObject = {
-  players: {},
-  deck: []
+    players: {},
+    deck: []
 };
 
 
 class Game {
-  constructor() {
-    this.reloadDeck();
-  }
-
-  reloadDeck() {
-    gameObject.deck = cards;
-
-  }
-
-  getDeckSize() {
-    return gameObject.deck.length;
-  }
-
-  createPlayerDeck() {
-    const playerHand = [];
-    for(let i = 0; i < 4; i++) {
-      let cardIndex = Math.floor(Math.random() * this.getDeckSize());
-
-      let card = gameObject.deck[cardIndex];
-
-      if (card.Rodzaj === 'Eksplodujący kotek') {
-        card = gameObject.deck[Math.floor(Math.random() * this.getDeckSize())];
-      }
-
-      playerHand.push(Object.assign({}, card));
-      gameObject.deck.splice(cardIndex, 1);
+    constructor() {
+        this.reloadDeck();
     }
 
-    playerHand.push({
-        "category": "Rozbrój",
-        "type": "disarm",
-        "description": "Kocią jogą ",
-        "action": "Umieść ostatnio wyciągniętą kartę z powrotem w talii",
-        "joke": " ",
-        "id": "51"
-    });
+    reloadDeck() {
+        gameObject.deck = cards;
 
-    return playerHand;
-  };
-
-
-  addPlayer(newPlayer) {
-    const playerId = findKey(gameObject.players, {name: newPlayer.name});
-    const id = playerId || uuid();
-
-    if (playerId) {
-      gameObject.players[id].sessionId = newPlayer.sessionId
-    } else {
-      gameObject.players[id] = {
-        sessionId: newPlayer.sessionId,
-        name: newPlayer.name,
-        id: id,
-        hand: this.createPlayerDeck()
-      }
     }
 
-    return gameObject.players[id];
-  }
+    getDeckSize() {
+        return gameObject.deck.length;
+    }
 
-  getPlayers() {
-    return values(mapValues(gameObject.players, (player) => {
-      const {id, name} = player
-      return {id, name};
-    }));
-  }
+    createPlayerDeck() {
+        const playerHand = [];
+        for (let i = 0; i < 4; i++) {
+            let cardIndex = Math.floor(Math.random() * this.getDeckSize());
+
+            let card = gameObject.deck[cardIndex];
+
+            if (card.Rodzaj === 'Eksplodujący kotek') {
+                card = gameObject.deck[Math.floor(Math.random() * this.getDeckSize())];
+            }
+
+            playerHand.push(Object.assign({}, card));
+            gameObject.deck.splice(cardIndex, 1);
+        }
+
+        playerHand.push({
+            "category": "Rozbrój",
+            "type": "disarm",
+            "description": "Kocią jogą ",
+            "action": "Umieść ostatnio wyciągniętą kartę z powrotem w talii",
+            "joke": " ",
+            "id": "51"
+        });
+
+        return playerHand;
+    };
+
+    getPlayerBySessionId(user) {
+
+    }
+
+    addPlayer(newPlayer) {
+        const playerId = findKey(gameObject.players, {name: newPlayer.name});
+        const id = playerId || uuid();
+
+        if (playerId) {
+            gameObject.players[id].sessionId = newPlayer.sessionId
+        } else {
+            gameObject.players[id] = {
+                sessionId: newPlayer.sessionId,
+                name: newPlayer.name,
+                id: id,
+                hand: this.createPlayerDeck()
+            }
+        }
+
+        return gameObject.players[id];
+    }
+
+    getPlayers() {
+        return values(mapValues(gameObject.players, (player) => {
+            const {id, name, hand} = player
+            return {id, name, numberOfCards: hand.length};
+        }));
+    }
+
+    getPlayer(user) {
+        console.log('user: ');
+        console.log(user);
+        console.log('players: ');
+        console.log(gameObject.players);
+        const key = findKey(gameObject.players, {sessionId: user.id})
+        return gameObject.players[key];
+    }
+
+    getCard(user) {
+        const player = this.getPlayer(user);
+        const card = gameObject.deck.pop();
+        player.hand.push(card);
+    }
 }
 
 const gameInstance = new Game();
